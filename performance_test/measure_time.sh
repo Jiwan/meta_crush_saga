4839b906e80f8c7c73374df0811fae013792cd5a
Mr User <29448170+aexg@users.noreply.github.com>
2018-02-18 19:31:22 +0100

    Simplify performance test script

--- performance_test/measure_time.sh
+++ performance_test/measure_time.sh
@@ -1,78 +1,45 @@
 #!/bin/sh
 
-n_measurements=10
-
 time_tool=/usr/bin/time
 perftest_path=./performance_test
 epoch_ms=1518906244300
 
 measure_build_time() {
+    game_field=$1
+    keypressed=$2
     cp $perftest_path/$game_field current_state.txt
     sum=0
-    i=0
-    printf "measuring "
+    printf "field=%-20s key=%-5s, measuring " $game_field $keypressed 
 
-    while [ $i -lt $n_measurements ]
+    for i in $(seq 1 10)
     do
         printf "."
         res[i]=$($time_tool -f %e g++ -std=c++1z main.cpp -DKEYBOARD_INPUT="$keypressed" -DEPOCH_MS="$epoch_ms" -o renderer 2>&1)
         sum=$(echo "$sum + ${res[i]}" | bc)
-        i=$(expr $i + 1)
     done
 
-    printf "\n"
     avg=$(echo "$sum / ${#res[@]}" | bc -l)
     
     variance=0
-    i=0
-
-    while [ $i -lt $n_measurements ]
+    for i in $(seq 1 ${#res[@]})
     do
         variance=$(echo "$variance + (${res[i]} - $avg) * (${res[i]} - $avg)" | bc -l)
-        i=$(expr $i + 1)
     done  
 
-    variance=$(echo "$variance / ($n_measurements - 1)" | bc -l)
+    variance=$(echo "$variance / (${#res[@]} - 1)" | bc -l)
     std=$(echo "sqrt($variance)" | bc -l)
     rsd=$(echo "100 * $std / $avg" | bc -l)
-    printf "field=%-20s key=%-5s: %0.3fs (%0.0f%%)\n" $game_field $keypressed $avg $rsd
+    printf "\ttime=%0.3fs (%0.0f%%)\n" $avg $rsd
 }
 
-
-game_field=default.txt
-keypressed='None'
-measure_build_time
-
-game_field=default.txt
-keypressed='Up'
-measure_build_time
-
-game_field=default.txt
-keypressed='Down'
-measure_build_time
-
-game_field=default.txt
-keypressed='Left'
-measure_build_time
-
-game_field=default.txt
-keypressed='Right'
-measure_build_time
-
-game_field=default.txt
-keypressed='Space'
-measure_build_time
-
-game_field=swapping.txt
-keypressed='Space'
-measure_build_time
-
-game_field=matched.txt
-keypressed='None'
-measure_build_time
-
-game_field=falldown.txt
-keypressed='None'
-measure_build_time
+measure_build_time default.txt 'None'
+measure_build_time default.txt 'Up'
+measure_build_time default.txt 'Down'
+measure_build_time default.txt 'Left'
+measure_build_time default.txt 'Right'
+measure_build_time default.txt 'Space'
+measure_build_time swapping.txt 'Space'
+measure_build_time matched.txt 'None'
+measure_build_time falldown.txt 'None'
 
 # [eof]
